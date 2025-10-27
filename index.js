b6dac0771134a3d81c3deee1d70b9879fcf21fd2
Dan Prince <1266011+danprince@users.noreply.github.com>
2025-10-27 11:36:45 +0100

    start UI on the board screen

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import { Game } from "./game.js";
-import { MenuScreen } from "./screens.js";
+import { generateBoard, STARTING_DECK } from "./campaign.js";
+import { Card, Game } from "./game.js";
+import { BoardScreen } from "./screens.js";
 import { UI } from "./ui.js";
 
 let game = new Game();
 
-UI.init(new MenuScreen(game));
+game.deck.addToTop(...STARTING_DECK.map((type) => new Card(type)));
+game.board = generateBoard(game);
+game.startRound();
+
+UI.init(new BoardScreen(game));
